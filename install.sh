73f32b4056478ab940be97903efd048ecf91f18b
Neil Ryan <soctar@gmail.com>
2017-05-31 09:31:07 -0700

    Removed upgrade/dist-upgrade on startup script Corrected git config install removes existing symlinks

--- install.sh
+++ install.sh
@@ -6,14 +6,22 @@ DOTFILES=$HOME/.files/
 sudo apt-get update
 sudo apt-get -y install make wget curl git
 sudo apt-get -y install zsh zsh-common vim vim-common vim-runtime
-sudo apt-get -y upgrade
-sudo apt-get -y dist-upgrade
+
 
 # Get oh-my-zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.zshrc ~/.zshrc.orig
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 chsh -s /bin/zsh
+
+
+# Remove symlinks if they exist
+rm -f $HOME/.zshrc
+rm -f $HOME/.gitignore
+rm -f $HOME/.gdbinit
+rm -f $HOME/.config/nvim/init.vim
+rm -f $HOME/.gitconfig
+rm -f $HOME/.vimrc
 
 # Setup symlinks so programs work
 ln -s $DOTFILES/zshrc $HOME/.zshrc
