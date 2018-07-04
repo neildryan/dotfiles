4c6ce34677972c7f66fcb3372c79e786c0dc839b
Neil Ryan <soctar@gmail.com>
2018-07-03 20:05:21 -0400

    Made the script work again

--- install-extra.sh
+++ install-extra.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
-#sudo add-apt-repository ppa:snwh/pulp
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+sudo add-apt-repository ppa:snwh/pulp
 sudo apt-get -y install audacity ardour hydrogen cecilia musescore
 sudo apt-get -y install texlive-full
 sudo apt-get -y install kdeconnect okular
@@ -13,7 +13,6 @@ sudo apt-get -y install htop
 sudo apt-get -y install keepassx pinta radare2
 sudo apt-get -y install paper-icon-theme paper-cursor-theme arc-theme
 sudo apt-get -y install jackd patchage qsynth
-sudo apt-get -y install teamviewer
 sudo snap install spotify
 
 echo "1)  google-chrome-stable"
